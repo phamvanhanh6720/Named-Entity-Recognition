33fe34e8a683e0b5bb034ef68ddc54f1fef055fe
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2022-03-11 00:00:18 +0700

    code data module

---
+++ dataset.py
@@ -0,0 +1,184 @@
+from typing import Optional
+
+import pandas as pd
+from typing import List
+from sklearn.model_selection import train_test_split
+
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS
+from transformers import AutoTokenizer
+from pytorch_lightning.core import LightningDataModule
+from torch.utils.data import Dataset
+
+from utils.dataset import NERDataSet
+
+
+class CustomDataset(Dataset):
+    def __init__(self,
+                 df: pd.DataFrame,
+                 model_name_or_path: str,
+                 tags_list: List[str],
+                 max_seq_length: int = 128,
+                 label_all_tokens: bool = False):
+
+        self.max_seq_length = max_seq_length
+        self.label_all_tokens = label_all_tokens
+
+        self.tokenzier = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+        self.tag2id = {}
+        for i in range(len(tags_list)):
+            self.tag2id[tags_list[i]] = i
+
+        self.dataset = []
+        for i in range(len(df)):
+            conll_format = df.iloc[i]['conll_label']
+            tokenized_inputs = self._tokenize_and_align_labels(data_point=conll_format)
+            self.dataset.append(tokenized_inputs)
+
+    def __len__(self):
+        len(self.dataset)
+
+    def __getitem__(self, idx):
+        item = self.dataset[idx]
+        input_ids = item['input_ids']
+        attention_mask = item['attention_mask']
+        labels = item['labels']
+
+        return input_ids, attention_mask, labels
+
+    def _tokenize_and_align_labels(self, data_point: List[str]):
+        word_list = [word.split(' ')[0] for word in data_point]
+        label_list = [word.split(' ')[-1] for word in data_point]
+
+        tokenized_inputs = self.tokenizer(word_list,
+                                          truncation=True,
+                                          is_split_into_words=True,
+                                          max_length=self.max_seq_length,
+                                          padding='max_length')
+        word_ids = tokenized_inputs.word_ids()
+
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:
+            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+            # ignored in the loss function.
+            if word_idx is None:
+                label_ids.append(-100)
+            # We set the label for the first token of each word.
+            elif word_idx != previous_word_idx:
+                label_ids.append(self.tag2id[label_list[word_idx]])
+            # For the other tokens in a word, we set the label to either the current label or -100, depending on
+            # the label_all_tokens flag.
+            else:
+                label_ids.append(self.tag2id[label_list[word_idx]] if self.label_all_tokens else -100)
+            previous_word_idx = word_idx
+
+        tokenized_inputs["labels"] = label_ids
+        return tokenized_inputs
+
+
+class NERDataModule(LightningDataModule):
+
+    def __init__(self,
+                 model_name_or_path: str,
+                 dataset_path: str,
+                 tags_list: List[str],
+                 max_seq_length: int = 128,
+                 train_batch_size: int = 32,
+                 eval_batch_size: int = 32,
+                 test_size: float = 0.1,
+                 val_size: float = 0.2,
+                 **kwargs):
+        super().__init__()
+
+        self.val_data = None
+        self.train_data = None
+        self.dataset_df = None
+        self.test_data = None
+
+        self.model_name_or_path = model_name_or_path
+        self.dataset_path = dataset_path
+        self.tags_list = tags_list
+
+        self.val_size = val_size
+        self.test_size = test_size
+
+        self.max_seq_length = max_seq_length
+        self.train_batch_size = train_batch_size
+        self.eval_batch_size = eval_batch_size
+
+    def prepare_data(self):
+        pass
+
+    def setup(self, stage: Optional[str] = None):
+        ner_dataset = NERDataSet(jsonl_file=self.dataset_path)
+        self.dataset_df = ner_dataset.dataset_df
+        df_train, df_rest = train_test_split(self.dataset_df,
+                                             shuffle=True,
+                                             random_state=43,
+                                             stratify=self.dataset_df[['source']],
+                                             train_size=1 - self.val_size - self.test_size)
+
+        df_val, df_test = train_test_split(df_rest,
+                                           shuffle=True,
+                                           random_state=43,
+                                           stratify=df_rest[['source']],
+                                           train_size=self.val_size / (self.val_size + self.test_size))
+
+        self.train_data = CustomDataset(df=df_train,
+                                        model_name_or_path=self.model_name_or_path,
+                                        tags_list=self.tags_list,
+                                        max_seq_length=self.max_seq_length,
+                                        label_all_tokens=True)
+
+        self.val_data = CustomDataset(df=df_val,
+                                      model_name_or_path=self.model_name_or_path,
+                                      tags_list=self.tags_list,
+                                      max_seq_length=self.max_seq_length,
+                                      label_all_tokens=True)
+
+        self.test_data = CustomDataset(df=df_test,
+                                       model_name_or_path=self.model_name_or_path,
+                                       tags_list=self.tags_list,
+                                       max_seq_length=self.max_seq_length,
+                                       label_all_tokens=True)
+
+    def train_dataloader(self):
+        pass
+
+    def val_dataloader(self):
+        pass
+
+    def test_dataloader(self):
+        pass
+
+    def predict_dataloader(self) -> EVAL_DATALOADERS:
+        pass
+
+
+if __name__ == '__main__ ':
+    ner_dataset = NERDataSet(jsonl_file='dataset/all_data_v1_02t03.jsonl')
+    dataset_df = ner_dataset.dataset_df
+
+    model = 'xlm-roberta-base'
+    tags_list = ["B-ADDRESS", "I-ADDRESS"
+                 "B-SKIILL", "I-SKILL",
+                 "B-EMAL", "I-EMAL",
+                 "B-PERSON", "I-PERSON",
+                 "B-PHONENUMBER", "I-PHONENUMBER",
+                 "B-QUANTITY", "I-QUANTITY",
+                 "B-PERSONTYPE", "I-PERSONTYPE",
+                 "B-ORGANIZATION", "I-ORGANIZATION",
+                 "B-PRODUCT", "I-PRODUCT",
+                 "B-IP", 'I-IP',
+                 "B-LOCATION", "I-LOCATION",
+                 "O",
+                 "B-DATETIME", "I-DATETIME",
+                 "B-EVENT", "I-EVENT",
+                 "B-URL", "I-URL"]
+
+
+    dataset = CustomDataset(df=dataset_df,
+                            model_name_or_path=self.model_name_or_path,
+                            tags_list=self.tags_list,
+                            max_seq_length=self.max_seq_length,
+                            label_all_tokens=True)
---
+++ utils/dataset.py
@@ -0,0 +1,215 @@
+import json
+import ast
+import numpy as np
+from typing import List
+
+import pandas as pd
+
+
+class NERDataSet:
+
+    def __init__(self, jsonl_file, entity_names=[]):
+
+        self.entity_names = ['PERSONTYPE', 'LOCATION', 'PHONENUMBER', 'EMAIL',
+                             'PRODUCT', 'URL', 'ORGANIZATION', 'DATETIME',
+                             'QUANTITY', 'ADDRESS', 'PERSON', 'SKILL',
+                             'EVENT', 'IP']
+
+        self.json_list = read_annotation_file(jsonl_file=jsonl_file)
+        self.all_entities = []
+        for json in self.json_list:
+            self.all_entities.append(self.extract_entities(data_point=json))
+
+        self.labels_dict = {'O': ['O', 'O']}
+        for name in self.entity_names:
+            self.labels_dict[name] = ['B-{}'.format(name), 'I-{}'.format(name)]
+
+        self._to_conll_list()
+        self._build_df()
+
+    def _build_df(self):
+        dataset = []
+        for i in range(len(self.conll_list)):
+            conll_label = self.conll_list[i]
+
+            json_data = self.json_list[i]
+            id_sen = json_data['id']
+            text = json_data['data']
+            ref_id = json_data['RED_ID'] if 'RED_ID' in json_data.keys() else None
+            source = json_data['SOURCE'] if 'SOURCE' in json_data.keys() else None
+
+            dataset.append([id_sen, ref_id, source, text, conll_label])
+
+        self.dataset_df = pd.DataFrame(data=dataset,
+                                       columns=['id', 'ref_id', 'source', 'text', 'conll_label'])
+
+    def _to_conll_list(self):
+
+        self.conll_list: List[List[str, str]] = []
+        for entities_list in self.all_entities:
+            syllable_level = []
+            for entity in entities_list:
+                text = entity[0]
+                label = entity[1]
+                if label == 'MISCELLANEOUS':
+                    continue
+
+                words = text.split(' ')
+                words = [word for word in words if word != '']
+
+                syllable_level.append(words[0].strip(' \n') + ' ' + self.labels_dict[label][0])
+                for word in words[1:]:
+                    syllable_level.append(word.strip(' \n') + ' ' + self.labels_dict[label][1])
+
+            self.conll_list.append(syllable_level)
+
+    @staticmethod
+    def extract_spans(labels_list: list, text_len):
+        spans_list = []
+
+        if not len(labels_list):
+            spans_list.append([0, text_len, 'O'])
+            return spans_list
+
+        if len(labels_list) == 1:
+            first_point = labels_list[0][0]
+            second_point = labels_list[0][1]
+            if first_point > 0:
+                spans_list.append([0, first_point, 'O'])
+            if second_point < text_len:
+                spans_list.append(labels_list[0])
+                spans_list.append([second_point, text_len, 'O'])
+
+            return spans_list
+
+        for i in range(0, len(labels_list) - 1):
+            span = labels_list[i]
+            next_label = labels_list[i + 1]
+
+            first_point = span[0]
+            second_point = span[1]
+            third_point = next_label[0]
+
+            if first_point > 0 and i == 0:
+                spans_list.append([0, first_point, 'O'])
+
+            spans_list.append(span)
+            if third_point <= second_point:
+                continue
+            else:
+                spans_list.append([second_point, third_point, 'O'])
+
+        last_tag = labels_list[-1]
+        if last_tag[1] < text_len:
+            spans_list.append(last_tag)
+            spans_list.append([last_tag[1], text_len, 'O'])
+        else:
+            spans_list.append(last_tag)
+
+        return spans_list
+
+    def extract_entities(self, data_point: dict) -> List[List[str]]:
+        entities_list = []
+        text = data_point['data']
+        labels_list: list = data_point['label']
+        sen_id = data_point['id']
+        source = data_point['SOURCE'] if 'SOURCE' in data_point.keys() else None
+
+        labels_list.sort(key=lambda x: x[0], reverse=False)
+
+        spans_list = self.extract_spans(labels_list=labels_list,
+                                        text_len=len(text))
+
+        for span in spans_list:
+            entity = text[span[0]: span[1]]
+            label = span[2]
+            entity = entity.strip(' \n')
+            if entity in ['', ' ']:
+                continue
+            else:
+                entities_list.append([entity, label, sen_id, source])
+
+        return entities_list
+
+
+def read_annotation_file(jsonl_file: str) -> List[dict]:
+    json_list = []
+
+    with open(jsonl_file, 'r') as file:
+        lines_list = file.readlines()
+        for line in lines_list:
+            item = json.loads(line)
+            if isinstance(item, dict):
+                if 'ID' in list(item.keys()):
+                    item['id'] = item['ID']
+                    item.pop('ID', None)
+                json_list.append(item)
+            elif isinstance(item, str):
+                item = ast.literal_eval(item)
+                if 'ID' in list(item.keys()):
+                    item['id'] = item['ID']
+                    item.pop('ID', None)
+                json_list.append(ast.literal_eval(item))
+
+    json_list.sort(key=lambda x: x['id'], reverse=False)
+
+    return json_list
+
+
+def compare_annotations(jsonl_file1, jsonl_file2):
+    json_list1 = read_annotation_file(jsonl_file1)
+    json_list2 = read_annotation_file(jsonl_file2)
+
+    id_list = []
+
+    if len(json_list1) != len(json_list2):
+        raise Exception('The number of sentences in 2 files is different')
+
+    for i in range(len(json_list1)):
+        sen1: dict = json_list1[i]
+        sen2: dict = json_list2[i]
+
+        if sen1['ID'] != sen2['ID']:
+            raise Exception('The content of 2 files is difference. File {} vs {}'.format(sen1['ID'], sen2['ID']))
+
+        id_sen = sen1['ID']
+
+        labels_1: list = sen1['label']
+        labels_2: list = sen2['label']
+
+        # Sort following start idx of span
+        labels_1.sort(key=lambda x: x[0], reverse=False)
+        labels_2.sort(key=lambda x: x[0], reverse=False)
+
+        if len(labels_1) != len(labels_2):
+            id_list.append(id_sen)
+            continue
+
+        for span_id in range(len(labels_1)):
+            span1 = labels_1[span_id]
+            span2 = labels_2[span_id]
+
+            entity_type1 = span1[2]
+            entity_type2 = span2[2]
+            if entity_type1 != entity_type2:
+                id_list.append(id_sen)
+                break
+
+            start_idx1 = span1[0]
+            start_idx2 = span2[0]
+            if start_idx1 != start_idx2:
+                id_list.append(id_sen)
+                break
+
+            end_idx1 = span1[1]
+            end_idx2 = span2[1]
+            if end_idx1 != end_idx2:
+                id_list.append(id_sen)
+                break
+
+    return id_list
+
+
+if __name__ == '__main__':
+    ner_dataset = NERDataSet(jsonl_file='../dataset/all_data_v1_02t03.jsonl')
+    print(1)
